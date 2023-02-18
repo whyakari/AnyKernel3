43117de55d64ac2ee526a1c27667ac0670a27b99
Akari <akariondev@gmail.com>
2023-02-18 17:14:40 +0000

    update banner and anykernel3.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=QuicksilveR Kernel by ghostrider-reborn
+kernel.string=HorikitaKernel by AkariOficial
 do.devicecheck=1
 do.modules=0
 do.systemless=1
